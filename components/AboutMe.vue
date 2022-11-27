67b4664e1b419faf801c001c3e72b7cfa6f4165e
Tony Jemba <jjembatony@gmail.com>
2022-11-27 11:21:57 +0300

    about me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
+  <div class="w-full ">
     <TheTitle heading="About Me" />
-    <p>
-      Hello! My name is Tony and I enjoy creating things that live on the internet.
-      My interest in web development started back in 2015 when i started my journey as a young
-      developer
-    </p>
+
+    <div class="w-11/12  flex justify-center mx-auto">
+      <div class=" w-10/12 flex justify-center mx-auto">
+        <div class="grow font-gsans">
+          <div class="w-5/6">
+            <p>
+              Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I
+              decided to try editing custom Tumblr themes — turns
+              out hacking together a custom reblog button taught me a lot about HTML & CSS!
+            </p>
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and
+              a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences
+              at Upstatement for a variety of clients.
+            </p>
+            <p>
+              I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+            </p>
+            <p>Here are a few technologies I’ve been working with recently:</p>
+          </div>
+        </div>
+        <div><ProfilePicture /></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +34,8 @@
 }
 .light-mode .headingColor {
   color: black;
+}
+.wid{
+  width: 78%;
 }
 </style>
---
+++ components/ProfilePicture.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useElementHover } from '@vueuse/core'
+
+const myHoverableElement = ref()
+const isHovered = useElementHover(myHoverableElement)
+</script>
+
+<template>
+  <div class="w-80 h-80 border-2 rounded bcolor relative">
+    <div ref="myHoverableElement" class="w-80 h-80 bpic bg-cover bg-no-repeat bg-center rounded absolute bottom-4 right-4 hue-rotate-15 grayscale hover:grayscale-0" :class="`${isHovered ? '-translate-y-2 -translate-x-2 duration-700 ' : 'translate-y-2 translate-x-2 duration-700 ease-in-out'}`" />
+  </div>
+</template>
+
+<style scoped>
+.bcolor{
+   border-color: #00FFE1 ;
+}
+.bpic {
+    background-image: url('~/assets/me.jpg');
+}
+</style>
