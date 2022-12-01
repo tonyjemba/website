f772e2552b288f2368aaf83369b5c82947af6be5
Tony Jemba <jjembatony@gmail.com>
2022-12-01 09:14:30 +0300

    naming convention2

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -7,7 +7,7 @@
         <div class="w-full md:grow font-gsans">
           <div class=" space-y-6">
             <p>
-              Hello! My name is Tony and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I
+              Hello! My name is New and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I
               decided to try editing custom Tumblr themes — turns
               out hacking together a custom reblog button taught me a lot about HTML & CSS!
             </p>
@@ -21,17 +21,17 @@
             </p>
             <p>Here are a few technologies I’ve been working with recently:</p>
             <div class="grid grid-cols-2  gap-y-2 text-sm ">
-              <SkillsList skill="JavaScript (TypeScript)" />
-              <SkillsList skill="Cypress & Jest" />
-              <SkillsList skill="React (Next)" />
-              <SkillsList skill="Git" />
-              <SkillsList skill="vue (Nuxt)" />
-              <SkillsList skill="Node.js" />
+              <AboutSkillsList skill="JavaScript (TypeScript)" />
+              <AboutSkillsList skill="Cypress & Jest" />
+              <AboutSkillsList skill="React (Next)" />
+              <AboutSkillsList skill="Git" />
+              <AboutSkillsList skill="vue (Nuxt)" />
+              <AboutSkillsList skill="Node.js" />
             </div>
           </div>
         </div>
         <div class="flex w-full justify-center md:justify-end">
-          <ProfilePicture />
+          <AboutProfilePicture />
         </div>
       </div>
     </div>
