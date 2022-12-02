98c8b8a6d7d41c3609868a4be371da66f01ba3e3
Tony Jemba <jjembatony@gmail.com>
2022-12-02 11:13:50 +0300

    contact 2

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,12 +1,43 @@
 <template>
   <TheTitle heading="Contact Me" underline-class="justify-start" />
   <div class="text-center">
-    <div class="w-11/12 sm:w-8/12 md:w-8/12 lg:w-5/12 mx-auto text-center  bg-red-900">
+    <div class="w-11/12 sm:w-8/12 md:w-8/12 lg:w-5/12 mx-auto text-center">
       <div class="font-gsans ">
         Although I’m not currently looking for any new opportunities, my inbox is
         always open. Whether you have a question or just want to say hi, I’ll try my
         best to get back to you!
       </div>
+      <div class="flex justify-center my-8">
+        <div class=" cursor-pointer textLcolor border rounded resume-btn">
+          <div class="text-center mt-3 mb-3 mr-5 ml-5 ">
+            Contact me
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.dark-mode .textLcolor {
+  color: #00FFE1;
+  transition: color .3s ease-in-out;
+}
+.textLcolor {
+  color: black;
+  transition: color .3s ease-in-out;
+}
+.textLcolor:hover {
+  color: #00FFE1;
+  transition: color .3s ease-in-out;
+}
+.resume-btn {
+  border-color: #00FFE1;
+  background-color: #00ffe100;
+  transition: background-color .3s ease-in-out;
+}
+.resume-btn:hover {
+  background-color: #00ffe11a;
+  transition: background-color .3s ease-in-out;
+}
+</style>
