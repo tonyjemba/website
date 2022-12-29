cfa991a51085cfd1e9382ecc9eee0e4f4f3969cc
Tony Jemba <jjembatony@gmail.com>
2022-12-29 11:20:32 +0300

    refactoring components3

--- components/AboutSkillsList.vue
+++ components/AboutSkillsList.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import Triangle from '~/assets/triangle.svg'
+
 interface Props {
   skill: String
 }
+
 const props = defineProps<Props>()
+
 </script>
 
 <template>
   <div class="flex items-center gap-x-3">
-    <div><Triangle /></div>
+    <div>
+      <Triangle />
+    </div>
     <div>{{ props.skill }}</div>
   </div>
 </template>
