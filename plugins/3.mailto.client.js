33e79e1c86996d5966ba35943baa9dd13a99ff71
Tony Jemba <jjembatony@gmail.com>
2022-12-31 01:18:56 +0300

    refactoring6

--- plugins/3.mailto.client.js
+++ plugins/3.mailto.client.js
@@ -3,5 +3,6 @@ export default defineNuxtPlugin((/* nuxtApp */) => {
   const script = document.createElement("script");
   script.src =
     "https://cdn.jsdelivr.net/npm/mariordev/mailtoui@1.0.3/dist/mailtoui-min.js";
+  script.async = "async";
   document.head.appendChild(script);
 });
