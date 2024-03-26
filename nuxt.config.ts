85140f3639cf3d082711c29145620152c7f75aec
Tony Jemba <jjembatony@gmail.com>
2024-03-26 06:26:35 +0300

    nuxt upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['nuxt-vercel-analytics', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss', 'nuxt-svgo', '@pinia/nuxt', '@vueuse/nuxt',
+  modules: ['nuxt-vercel-analytics', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss', 'nuxt-svgo', '@pinia/nuxt', '@vueuse/nuxt', '@nuxtjs/seo',
     ['@nuxtjs/google-fonts', {
       families: {
         'Open+Sans': [400, 500],
         'Jost': [500],
       },
     }]],
+
   colorMode: {
     preference: 'dark', // default value of $colorMode.preference
     fallback: 'dark', // fallback value if not system preference found
   },
+
   app: {
     head: {
       charset: 'utf-16',
@@ -23,7 +25,7 @@ export default defineNuxtConfig({
         {
           name: 'description',
           hid: 'description',
-          content: 'As a passionate and experienced software engineer, I am dedicated to delivering innovative and efficient solutions that meet the needs of modern businesses.',
+          content: 'My name is Tony Jemba a passionate and experienced software engineer, I am dedicated to delivering innovative and efficient solutions that meet the needs of modern businesses.',
         },
         {
           name: 'og: title',
@@ -33,7 +35,7 @@ export default defineNuxtConfig({
         {
           name: 'og: description',
           hid: 'og: description',
-          content: 'As a passionate and experienced software engineer, I am dedicated to delivering innovative and efficient solutions that meet the needs of modern businesses.',
+          content: 'My name is Tony Jemba a passionate and experienced software engineer, I am dedicated to delivering innovative and efficient solutions that meet the needs of modern businesses.',
         },
 
       ],
@@ -41,8 +43,20 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
     },
   },
+
   typescript: {
     strict: true,
   },
 
+  site: {
+    // production URL
+    url: 'https://tonyjemba.com',
+    name: 'Tony Jemba',
+  },
+
+  devtools: {
+    timeline: {
+      enabled: true,
+    },
+  },
 })
